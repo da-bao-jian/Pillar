edc3ba429bf821953ab1dac5a4614562a29c59e1
harry <gaohaoyuandd@gmail.com>
2020-12-29 00:19:59 -0500

    fixed logout button

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,21 +6,14 @@ import MainPage from './main/main_page';
 import ChatBox from './chat/chat_box_container';
 import DashBoard from './chat/dashboard_container';
 import SplashPageContainer from './splash/splash_page_container';
-import SideBar from './chat/side_bar_container';
+import NavBar from './nav/navbar_container';
 
 const App = () => (
   <div>
     <Modal />
     {/* <Route path="/" component={SideBar} /> */}
-    {/* <SideBar /> */}
+    <NavBar />
     <Switch>
-{/* <<<<<<< HEAD
-        <AuthRoute exact path="/login" component={LoginFormContainer} />
-        <AuthRoute exact path="/signup" component={SignupFormContainer} />
-        <AuthRoute exact path="/" component={MainPage} />
-        <ProtectedRoute exact path="/chat" component={DashBoard} /> 
-======= */}
-      {/* <Route exact path="/" component={MainPage} /> */}
       <AuthRoute exact path="/" component={SplashPageContainer} />
       <ProtectedRoute exact path="/chat" component={DashBoard} />
     </Switch>
--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -24,34 +24,15 @@ class NavBar extends React.Component {
             </div>
         );
       } else {
-        let current_path = this.props.location.pathname.split('/');
-        if(current_path.includes('login') && current_path.length>1){
           return (
-            <div>
-                <Link to={'/signup'}>Signup</Link>
-            </div>
+            null
           )
-        } else if (current_path.includes('signup') && current_path.length>1){
-          return (
-              <div>
-                <Link to={'/login'}>Login</Link>
-              </div>
-          );
-        } else {
-            return (
-              <div>
-                  <Link to={'/signup'}>Signup</Link>
-                  <Link to={'/login'}>Login</Link>
-              </div>
-          );
-        }
       }
   }
 
   render() {
       return (
         <div>
-          <h1>Pillar</h1>
             { this.getLinks() }
         </div>
       );
